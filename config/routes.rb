f1eaff4c709f64fd590c27fad6dbec4b1ec2b0e5
HannaKitora <h.wimper@gmail.com>
2024-05-04 06:53:47 +0000

    [Finish]11-5

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   
   get 'lists/:id' => 'lists#show', as: 'list'
   
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 
 
 end
